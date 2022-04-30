0f196c1249a6a06263ce95d1ff510ffea5936f67
Maou-Shimazu <eldaddanladi@gmail.com>
2022-04-30 14:37:47 -0700

    fixed process spawn

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,32 @@ use std::process::Command;
 use colored::Colorize;
 
 fn main() -> std::io::Result<()> {
-    let dir = std::env::current_dir()?;
+    let mut dir = std::env::current_dir()?;
     println!("{}: {}", "Current directory".cyan(), dir.display());
-    let paths = fs::read_dir(dir).unwrap();
+    let paths = fs::read_dir(dir.clone()).unwrap();
     let mut vec: Vec<String> = Vec::new();
 
     for path in paths {
         vec.push(path.unwrap().path().to_str().unwrap().to_owned());
     }
-
+    dir.push(".git");
+    let _git = std::fs::metadata(dir)?;
     for i in vec {
         if i.as_str().contains("\\") && !(i.as_str().contains(".")) {
-                println!("{} {}", "Pulling".green(), i);
-                let _cd = Command::new(format!("cd {}", i).as_str()).output();
-                let _pull = Command::new("git pull").output();
+                std::env::set_current_dir(i.clone()).ok();
+
+            if _git.is_dir() {
+            println!("{}", "Github Directory Detected.".green());
+            println!("{} {}", "Pulling".green(), i);
+
+                #[cfg(windows)]
+                Command::new("powershell").args(["/c", "git pull"]).output().expect("Failed to git pull");
+
+                #[cfg(unix)]
+                Command::new("sh").args(["-c", "git pull"]).output().expect("Failed to git pull");
+            }
+
+                //format!("cd {}", i).as_str()).output();
         }
         else { println!("{}: {}", "Private folder or File".yellow(), i); }
 
